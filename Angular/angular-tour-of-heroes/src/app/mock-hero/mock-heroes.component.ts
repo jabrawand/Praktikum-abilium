1848bf856eb9d05cee6ec73668f6b0d1d0992c9a
Jasmin <jasmin_brawand@hotmail.com>
2024-11-25 18:14:11 +0100

    create and delete mock-hero

---
+++ Angular/angular-tour-of-heroes/src/app/mock-hero/mock-heroes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-hero',
+  templateUrl: './mock-hero.component.html',
+  styleUrl: './mock-hero.component.css'
+})
+export class MockHeroComponent {
+
+}
