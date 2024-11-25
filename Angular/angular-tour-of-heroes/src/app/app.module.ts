550aaa7186930bca193e05293938dc9008bf758e
Jasmin <jasmin_brawand@hotmail.com>
2024-11-25 17:23:13 +0100

    deleted import {AppRoutingModule} in app.module.ts

--- Angular/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
