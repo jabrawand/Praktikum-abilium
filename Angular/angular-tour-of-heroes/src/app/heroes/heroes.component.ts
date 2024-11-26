2ebac968556d02fd83a045e66b64df1c55fcc173
Jasmin <jasmin_brawand@hotmail.com>
2024-11-26 22:06:50 +0100

    added click event handler

--- Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -19,4 +19,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class HeroesComponent {
   heroes = HEROES;
+  selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
