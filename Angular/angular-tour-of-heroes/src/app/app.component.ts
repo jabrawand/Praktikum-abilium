ac861e3dd2436abaade22d503507ad850905da88
Jasmin <jasmin_brawand@hotmail.com>
2024-11-26 22:02:11 +0100

    add a click event binding again

--- Angular/angular-tour-of-heroes/src/app/app.component.ts
+++ Angular/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-/* Frage: Im Beispiel steht standalone: true; wenn ich dies einfüge, funktioniert ng serve --open nicht mehr?!*/
+
 @Component({
-  selector: 'app-root', 
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
