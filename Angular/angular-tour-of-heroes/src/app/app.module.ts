6e3f2934a377893bc85f25340ed6ba0772c193b5
Jasmin <jasmin_brawand@hotmail.com>
2024-12-05 08:31:38 +0100

    add a dashboard

--- Angular/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
