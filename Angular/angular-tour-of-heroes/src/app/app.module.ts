744c169464f09d597c5135eca8d196de1461c388
Jasmin <jasmin_brawand@hotmail.com>
2024-12-18 19:47:53 +0100

    imported HttpClient in hero.service.ts

--- Angular/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -24,7 +24,13 @@ import { InMemoryDataService } from './in-memory-data.service';
     HeroDetailComponent,
     AppRoutingModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule
+
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+)
     
   ],
   providers: [],
