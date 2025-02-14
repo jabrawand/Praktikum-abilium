394fa957d09623955249cf88bef7e06099f42d5a
jabrawand <jasmin_brawand@hotmail.com>
2025-02-14 18:04:13 +0100

    install odoo basic

---
+++ Odoo/odoo/addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Estate',
+    'version': '18.0.0.1',
+    'depends': ['base'],
+    'category': 'Real Estate',
+    'summary': '',
+    'description': '',
+    'author': 'Jasmin',
+  
+    'data': [],
+   
+}
---
+++ Odoo/odoo/addons/mein_erstes_modul/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mein erstes Modul",
+
+    'summary': """Beschreibung""",
+
+    'author': "Abilium GmbH",
+    'website': "https://www.abilium.io",
+
+    'category': 'Productivity',
+    'version': '18.0.0.1',
+
+    'depends': [
+        'contacts',
+    ],
+
+    'data': [
+        'views/res_partner.xml'
+    ],
+
+    'assets': {
+        
+    },
+}
