a64bdbe8dc0be913fcaaf8581d13df6e0775f37f
jabrawand <jasmin_brawand@hotmail.com>
2025-10-23 05:52:56 +0200

    Added Odoo project

---
+++ demo_shop/addons/demo_shop/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Demo Shop',
+    'version': '1.0',
+    'depends': [
+        'base',
+        'sale',
+        'project'
+        ],
+    'author': 'Jasmin',
+    'cathegory': 'Demo Shop',
+    'description': """
+    Demo Shop for testing""",
+    'data': [
+        'views/project_task.xml'
+
+    ],
+    'application': True,
+    'license': 'LGPL-3'
+    
+}
